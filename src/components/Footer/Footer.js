4687b05ba04958280919d659d9bed83725107e05
Pilar Gomez <pilarg@gmail.com>
2022-11-07 23:32:11 -0300

    ultimos detalles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,22 @@
 import "./footer.css";
+import { AiFillCopyrightCircle } from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
 
 function Footer () {
     return (
         <div className="footer">
             <div className="title-redes">
-                    <h6>Seguinos en Redes Sociales</h6>
-                    <h6> redes </h6>
+                <h6>Seguinos en Redes Sociales</h6>
+                <h6> redes </h6>
             </div>
+            
+            
+            <div className="copy-right">    
+            <h6><FaHeart/> <AiFillCopyrightCircle/> PG Web Development -  2022 <FaHeart/></h6>
+            </div>
+            
         </div>
+        
     );
 }
  
